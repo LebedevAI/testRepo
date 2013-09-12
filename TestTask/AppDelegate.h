10f77b207bc7b9bd76be03a43629fbcaa5a84a0e
Alexander Lebedev <alexander.lebedev@2nova.ru>
2013-09-12 13:55:53 +0400

    + project

---
+++ TestTask/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestTask
+//
+//  Created by Alexander Lebedev on 9/12/13.
+//  Copyright (c) 2013 Alexander Lebedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestTask/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestTask
+//
+//  Created by Alexander Lebedev on 9/12/13.
+//  Copyright (c) 2013 Alexander Lebedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
